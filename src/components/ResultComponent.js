08ab4f8d70fe61103c66293de44cbbaf87aad9e5
Cedeno Hernandez <cedeno.hernandez@gmail.com>
2019-11-14 15:41:14 -0800

    Add styling

--- src/components/ResultComponent.js
+++ src/components/ResultComponent.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
 
 class ResultComponent extends Component {
-    
+
     render() {
         let {result} = this.props;
         return (
-            <div className="result">
+            <div className="display">
                 <p>{result}</p>
             </div>
         )
